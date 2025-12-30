889a0592cf8ef4e5a490c5c4425926e0abeb24f6
LearnCreateApply <shivamingulkar1234@gmail.com>
2025-12-30 22:48:39 +0530

    started with ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-
-import './App.css';
-import { Link } from 'react-router-dom';
-import { useAuth } from './context/AuthContext';
-import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom';
+import "./App.css";
+import { Link } from "react-router-dom";
+import { useAuth } from "./context/AuthContext";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 function App() {
-  const {loading , user } = useAuth()
-  const navigate = useNavigate()
- useEffect(() => {
+  const { loading, user } = useAuth();
+  const navigate = useNavigate();
+  useEffect(() => {
     if (!loading) {
       if (user) {
         navigate("/dashboard");
@@ -16,8 +15,25 @@ function App() {
   }, []);
   return (
     <div>
-      <Link to={'/auth'} >Sign Up</Link>
-      <Link to={'/auth'} >Log in</Link>
+      <div className="navbarsection">
+        <div className="logo monebold">StudentOS</div>
+        <div className="navbtn">
+          <Link to={"/auth"}>Sign Up</Link>
+          <Link to={"/auth"}>Log in</Link>
+        </div>
+      </div>
+      <div className="herosection">
+        <div className="herotitle">
+          <div className="monebold"
+            
+          >
+            Stay Focused
+          </div>
+          <div className="monebold" style={{
+              color: "#1D4ED8",
+            }}>Be Productive</div>
+        </div>
+      </div>
     </div>
   );
 }
